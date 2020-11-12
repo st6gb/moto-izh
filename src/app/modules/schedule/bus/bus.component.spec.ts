4cb99b11a9cae8c4066f94027c8990bde107d18d
st6gb <pavel_buldakov@yahoo.com>
2020-11-12 11:37:32 +0400

    update angular 11

--- src/app/modules/schedule/bus/bus.component.spec.ts
+++ src/app/modules/schedule/bus/bus.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { BusComponent } from './bus.component';
 
@@ -6,7 +6,7 @@ describe('BusComponent', () => {
   let component: BusComponent;
   let fixture: ComponentFixture<BusComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ BusComponent ]
     })
