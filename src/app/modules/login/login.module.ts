8e7eb62db2cc321d113c373c7f2b1e6e5d1b4c0b
pavel <pavel_buldakov@yahoo.com>
2022-03-22 08:11:01 +0400

    wip

--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -11,6 +11,7 @@ import { LoginComponent } from './login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RegistrationComponent } from './registration/registration.component';
 import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from './auth.service';
 
 
 @NgModule({
@@ -28,6 +29,7 @@ import { HttpClientModule } from '@angular/common/http';
     MatIconModule,
     MatButtonModule,
     MatProgressSpinnerModule,
-  ]
+  ],
+  providers: [AuthService]
 })
 export class LoginModule { }
