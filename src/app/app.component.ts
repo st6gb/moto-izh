164044278516d6eda39db3b06200babdc0ba349b
Buldakov Pavel <st6gb@Buldakovs-MacBook-Pro.local>
2026-06-03 20:39:30 +0400

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { concat, map, ReplaySubject, Subject, withLatestFrom, zip } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -20,24 +19,6 @@ export class App implements OnInit {
   }
 
   ngOnInit(): void {
-    console.log('here');
-    const fistSubject = new ReplaySubject(1);
-    const secondSubject = new ReplaySubject(1);
-    const dad = new Subject();
 
-    dad.subscribe(console.log);
-    dad.next(123);
-    concat(fistSubject, secondSubject).pipe(
-    )
-    .subscribe((a) => console.log(a));
-
-    fistSubject.next(true);
-    secondSubject.next(false);
-
-    secondSubject.next(true);
-    fistSubject.next(false);
-
-    secondSubject.next(false);
-    secondSubject.next(true);
   }
 }
